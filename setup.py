ef4a79f2d4c0e546a4cb4ac6de64cd611db740d7
charaneesh20uari041@mahindrauniversity.edu.in <sampathkumar@Charaneeshs-Macbook-Pro.local>
2024-06-08 18:34:26 +0530

    EDA and Model Training

--- setup.py
+++ setup.py
@@ -1,24 +1,27 @@
 from setuptools import find_packages,setup
-from aifc import open
 from typing import List
 
 HYPEN_E_DOT='-e .'
 def get_requirements(file_path:str)->List[str]:
-    
-    requirements = []
+    '''
+    this function will return the list of requirements
+    '''
+    requirements=[]
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
         requirements=[req.replace("\n","") for req in requirements]
-        
+
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
     
     return requirements
+
 setup(
-    name='MLProject',
-    version='0.0.1',
-    author='CR',
-    email='charaneesh20uari041@mahindrauniversity.edu.in',
-    packages=find_packages(),
-    install_requires=get_requirements('requirements.txt')
+name='mlproject',
+version='0.0.1',
+author='CR',
+author_email='charan789456@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+
 )
